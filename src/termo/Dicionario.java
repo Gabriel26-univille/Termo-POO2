0d56c9a14b74f4860c6d98c6bda9a8390c333199
Bruno <50593937+brunorichartzf@users.noreply.github.com>
2023-11-26 19:12:33 -0300

    Implementação do dicionário completo
    Implementação do dicionário com todas as palavras de cinco letras presentes na lista entregue pelo professor. Palavras de tamanhos diferentes foram pré-removidas com script.
    
    Co-Authored-By: Gabriel26-univille <132339190+Gabriel26-univille@users.noreply.github.com>

--- src/termo/Dicionario.java
+++ src/termo/Dicionario.java
@@ -1,20 +1,33 @@
 package termo;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Dicionario {
 
     //lista de palavras (manter tudo minúsculo e sem acento)
-    final String[] palavras = {
-            "fazer",
-            "assim",
-            "vigor",
-            "sutil",
-            "aquem",
-            "porem",
-            "seçao",
-            "fosse",
-            "sanar",
-            "poder"
-    };
+    final String[] palavras = lerArquivo("resources/dictionary.txt");
+
+        public static String[] lerArquivo(String filename) {
+            List<String> lines = new ArrayList<>();
+            try {
+                Scanner scanner = new Scanner(new File(filename));
+
+                while (scanner.hasNextLine()) {
+                    lines.add(scanner.nextLine());
+                }
+
+                scanner.close();
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+
+            return lines.toArray(new String[0]);
+        }
 
     //checa se o input é uma palavra de verdade (que consta no array de palavras)
     public boolean dicionarioCheck(String input){
