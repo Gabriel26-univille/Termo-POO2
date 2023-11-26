48c88869040c7629c0d106307569bd487aba8993
Bruno <50593937+brunorichartzf@users.noreply.github.com>
2023-11-26 19:34:36 -0300

    Line cleanup
    Co-Authored-By: Gabriel26-univille <132339190+Gabriel26-univille@users.noreply.github.com>

--- src/termo/WordChecker.java
+++ src/termo/WordChecker.java
@@ -13,31 +13,24 @@ public class WordChecker {
     public int checarPalavra(String input, String resposta, int[] status) {
         int pontos = 0;
 
-        //statusResposta limita a quantidade de meio certos (amarelo) que podem aparecer em uma palavra
-        //um 1 simboliza que a letra nessa posicao ja foi marcada como meio certo em algum momento
-        //primeiro array se refere ao input e o segundo a resposta
-        //o primeiro array nao pode ter mais meio certos que o segundo
         int[][] statusResposta = {{0,0,0,0,0}, {0,0,0,0,0}};
 
         for(int tentativaIndex = 0; tentativaIndex < input.length(); tentativaIndex++){
             for(int respostaIndex = 0; respostaIndex < resposta.length(); respostaIndex++){
 
-                //se as letras comparadas sao iguais
                 if(input.charAt(tentativaIndex) == resposta.charAt(respostaIndex)){
 
                     if (statusResposta[1][respostaIndex] == 0){
                         statusResposta[1][respostaIndex] = 1;
                     }
 
-                    //se letra está no mesmo lugar e há espaço para mais um certo || meio certo
                     if(tentativaIndex == respostaIndex && somaArray(statusResposta[1]) > somaArray(statusResposta[0])){
 
                         pontos += 2;
                         status[tentativaIndex] = 2;
                         statusResposta[0][tentativaIndex] = 1;
                         break;
-
-                        //se letra está no lugar errado e há espaço para mais um certo || meio certo
+                        
                     } else if (somaArray(statusResposta[1]) > somaArray(statusResposta[0])){
 
                         pontos += 1;
